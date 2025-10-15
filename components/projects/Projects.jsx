16f2dec71f3a8f2159ae95b81c4a9704b9ed7eb6
ArturMan <artur.mrteyan@beeonco.de>
2025-10-15 19:06:16 +0400

    projc

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -1,9 +1,23 @@
+"use client";
+
 import React from 'react';
+import SpotlightCard from "@/components/bits/SpotlightCard";
 
 function Projects({data}) {
   const {projects} = data;
+  console.log(projects, 'projects');
+
   return (
-    <section></section>
+    <section className="px-[10vw] py-[5vw] flex justify-between">
+      {projects?.map((project) => {
+        return (
+          <SpotlightCard key={project?.id} className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+            <h1>Name</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, id repudiandae? Aliquid deleniti dolor ea, eos fuga fugiat hic illo in iusto libero omnis quaerat quasi qui sapiente, similique voluptas? Blanditiis, laudantium magnam. Doloremque, praesentium, saepe! Ad assumenda commodi consectetur culpa debitis deleniti, eius exercitationem facilis illum inventore iure laboriosam nisi quo, veniam voluptatum? Cumque dolorem maxime modi perspiciatis quo.</p>
+          </SpotlightCard>
+        )
+      })}
+    </section>
   );
 }
 
--- db/projects/ProjectsDB.js
+++ db/projects/ProjectsDB.js
@@ -1,11 +1,67 @@
-const ProjectsDB = {
-	id: 1,
-	status: 1,
-	translation: [
-		{name: "Իմ Մասին", description: `Ես Front-End Վեբ Ծրագրավորող եմ՝ React.js, Next.js, TypeScript և Tailwind CSS-ի միջոցով արագ արձագանքող, մասշտաբային հավելվածներ ստեղծելու 50+ տարվա գործնական փորձով։ Ես կենտրոնանում եմ մաքուր կոդի և ինտուիտիվ օգտագործողի ինտերֆեյսների ստեղծման վրա։`, key: 'am'},
-		{name: "About Me", description: `I’m a Front-End Web Developer with 50+ years of hands-on experience building responsive, scalable applications using React.js, Next.js, TypeScript, and Tailwind CSS. I focus on creating clean code and intuitive user interfaces.`, key: 'en'},
-		{name: "Обо Мне", description: `Я Front-End Веб-разработчик с более чем 50 практическим опытом создания адаптивных масштабируемых приложений с использованием React.js, Next.js, TypeScript и Tailwind CSS. Я стремлюсь создавать чистый код и интуитивно понятные пользовательские интерфейсы.`, key: 'ru'},
-	]
-};
+const ProjectsDB = [
+  {
+    id: 1,
+    status: 1,
+    translation: [
+      {
+        name: "Իմ Մասին",
+        description: `Ես Front-End Վեբ Ծրագրավորող եմ՝ React.js, Next.js, TypeScript և Tailwind CSS-ի միջոցով արագ արձագանքող, մասշտաբային հավելվածներ ստեղծելու 50+ տարվա գործնական փորձով։ Ես կենտրոնանում եմ մաքուր կոդի և ինտուիտիվ օգտագործողի ինտերֆեյսների ստեղծման վրա։`,
+        key: 'am'
+      },
+      {
+        name: "About Me",
+        description: `I’m a Front-End Web Developer with 50+ years of hands-on experience building responsive, scalable applications using React.js, Next.js, TypeScript, and Tailwind CSS. I focus on creating clean code and intuitive user interfaces.`,
+        key: 'en'
+      },
+      {
+        name: "Обо Мне",
+        description: `Я Front-End Веб-разработчик с более чем 50 практическим опытом создания адаптивных масштабируемых приложений с использованием React.js, Next.js, TypeScript и Tailwind CSS. Я стремлюсь создавать чистый код и интуитивно понятные пользовательские интерфейсы.`,
+        key: 'ru'
+      },
+    ]
+  },
+  {
+    id: 2,
+    status: 1,
+    translation: [
+      {
+        name: "Իմ Մասին",
+        description: `Ես Front-End Վեբ Ծրագրավորող եմ՝ React.js, Next.js, TypeScript և Tailwind CSS-ի միջոցով արագ արձագանքող, մասշտաբային հավելվածներ ստեղծելու 50+ տարվա գործնական փորձով։ Ես կենտրոնանում եմ մաքուր կոդի և ինտուիտիվ օգտագործողի ինտերֆեյսների ստեղծման վրա։`,
+        key: 'am'
+      },
+      {
+        name: "About Me",
+        description: `I’m a Front-End Web Developer with 50+ years of hands-on experience building responsive, scalable applications using React.js, Next.js, TypeScript, and Tailwind CSS. I focus on creating clean code and intuitive user interfaces.`,
+        key: 'en'
+      },
+      {
+        name: "Обо Мне",
+        description: `Я Front-End Веб-разработчик с более чем 50 практическим опытом создания адаптивных масштабируемых приложений с использованием React.js, Next.js, TypeScript и Tailwind CSS. Я стремлюсь создавать чистый код и интуитивно понятные пользовательские интерфейсы.`,
+        key: 'ru'
+      },
+    ]
+  },
+  {
+    id: 3,
+    status: 1,
+    translation: [
+      {
+        name: "Իմ Մասին",
+        description: `Ես Front-End Վեբ Ծրագրավորող եմ՝ React.js, Next.js, TypeScript և Tailwind CSS-ի միջոցով արագ արձագանքող, մասշտաբային հավելվածներ ստեղծելու 50+ տարվա գործնական փորձով։ Ես կենտրոնանում եմ մաքուր կոդի և ինտուիտիվ օգտագործողի ինտերֆեյսների ստեղծման վրա։`,
+        key: 'am'
+      },
+      {
+        name: "About Me",
+        description: `I’m a Front-End Web Developer with 50+ years of hands-on experience building responsive, scalable applications using React.js, Next.js, TypeScript, and Tailwind CSS. I focus on creating clean code and intuitive user interfaces.`,
+        key: 'en'
+      },
+      {
+        name: "Обо Мне",
+        description: `Я Front-End Веб-разработчик с более чем 50 практическим опытом создания адаптивных масштабируемых приложений с использованием React.js, Next.js, TypeScript и Tailwind CSS. Я стремлюсь создавать чистый код и интуитивно понятные пользовательские интерфейсы.`,
+        key: 'ru'
+      },
+    ]
+  },
+];
 
 export default ProjectsDB;
